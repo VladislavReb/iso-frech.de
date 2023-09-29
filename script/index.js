5ba7f9cbd67c50c0a1037391b5b6c5100074ef9e
VladislavReb <104519014+VladislavReb@users.noreply.github.com>
2023-09-29 13:41:18 +0200

    add new photo in 4th section

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-// Main page, service hover logic.
+// Home page, service hover logic.
 const serviceCards = document.querySelectorAll('.ts__service');
 
 serviceCards.forEach((elem) => {
@@ -17,3 +17,26 @@ serviceCards.forEach((elem) => {
         document.querySelectorAll('.active').forEach(elem => elem.style.display = 'none');
     })
 })
+
+
+
+// Home page, photo animation.
+function comePhotoBlocks (entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            document.querySelector('.fs__img__1').style.animation = 'block 2.5s forwards';
+            document.querySelector('.fs__img__2').style.animation = 'block_2 2.5s forwards';
+            observer.unobserve(entry.target);
+        }
+    });
+}
+
+const targetElem = document.querySelector('.fs__img');
+const optionsRender = {
+    root: null,
+    threshold: 0,
+};
+
+const observerSecond = new IntersectionObserver(comePhotoBlocks, optionsRender);
+
+observerSecond.observe(targetElem);
