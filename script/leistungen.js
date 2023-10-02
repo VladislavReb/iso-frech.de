5ba4de8acbcfcc5c8beeea230bade74c2b4bc19e
VladislavReb <104519014+VladislavReb@users.noreply.github.com>
2023-10-02 13:48:17 +0200

    made service page

--- script/leistungen.js
+++ script/leistungen.js
@@ -1,69 +1,22 @@
-// Service photo animation.
-const optionsRender = {
-    root: null,
-    threshold: 0,
-};
+// Show hidden service photo.
+document.querySelectorAll('.service__arrow').forEach(elem => {
+    elem.addEventListener('click', ({target}) => {
+        target.parentElement.nextElementSibling.classList.toggle('active_photo');
+        target.classList.toggle('rotate__arrow');
+        target.stopImmediatePropagation();
+    })
+})
 
-//1. wärmeschutz
-function comePhotoWarm (entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            document.querySelector('.wärmeschutz_1').style.animation = 'block 2.5s forwards';
-            document.querySelector('.wärmeschutz_2').style.animation = 'block_2 2.5s forwards';
-            observer.unobserve(entry.target);
-        }
-    });
-}
 
-const targetElem = document.getElementById('wärmeschutz');
-const observerSecond = new IntersectionObserver(comePhotoWarm, optionsRender);
+// Referenzen carousel.
+const boxServices = document.querySelector('.image_slider');
 
-observerSecond.observe(targetElem);
+const nextButton = document.querySelector('.array');
+nextButton.addEventListener("click", () => {
+    return boxServices.scrollBy(425, 0);
+});
 
-//2. kälteschutz
-function comePhotoKalt (entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            document.querySelector('.kälteschutz_1').style.animation = 'block 2.5s forwards';
-            document.querySelector('.kälteschutz_2').style.animation = 'block_2 2.5s forwards';
-            observer.unobserve(entry.target);
-        }
-    });
-}
-
-const targetElemKalt = document.getElementById('kälteschutz');
-const observerSecondKalt = new IntersectionObserver(comePhotoKalt, optionsRender);
-
-observerSecondKalt.observe(targetElemKalt);
-
-//3. schallschutz
-function comePhotoSchall (entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            document.querySelector('.schallschutz_1').style.animation = 'block 2.5s forwards';
-            document.querySelector('.schallschutz_2').style.animation = 'block_2 2.5s forwards';
-            observer.unobserve(entry.target);
-        }
-    });
-}
-
-const targetElemSchall = document.getElementById('schallschutz');
-const observerSecondSchall = new IntersectionObserver(comePhotoSchall, optionsRender);
-
-observerSecondSchall.observe(targetElemSchall);
-
-//4.
-function comePhotoBrand (entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            document.querySelector('.brandschutzauflagen_1').style.animation = 'block 2.5s forwards';
-            document.querySelector('.brandschutzauflagen_2').style.animation = 'block_2 2.5s forwards';
-            observer.unobserve(entry.target);
-        }
-    });
-}
-
-const targetElemBrand = document.getElementById('brandschutzauflagen');
-const observerSecondBrand = new IntersectionObserver(comePhotoBrand, optionsRender);
-
-observerSecondBrand.observe(targetElemBrand);
+const prevButton = document.querySelector('.array_2');
+prevButton.addEventListener("click", () => {
+    return boxServices.scrollBy(-425, 0);
+});
