92a95c1085d601973511881490542286ca1049a6
VladislavReb <104519014+VladislavReb@users.noreply.github.com>
2023-10-09 12:03:35 +0200

    fix animation

--- script/leistungen.js
+++ script/leistungen.js
@@ -20,6 +20,31 @@ $(document).ready(function(){
         prevArrow: '.array_2',
         variableWidth: true,
         speed: 500,
+        responsive: [
+            {
+                breakpoint: 1365,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
 
